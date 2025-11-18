28df86fedf80ce4e455bd33aa99692fe26592429
gabrielsiewert <gabrielsiewert1@gmail.com>
2025-11-18 18:20:31 -0500

    Added WebGL build.

---
+++ Build/Builds.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8148eb11f66d90b3efa56b7797e18b90192b477dea6c5f653247da4ef33c9c89
+size 415159
---
+++ TemplateData/scorm.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9f6c5405f02a263e5ce13f569e051afcf2139dee927a5b43752a36b3bb16fe1e
+size 20675
